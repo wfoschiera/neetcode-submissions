2b5f92160b2d626fb26bea1a966b5aa5bd08072d
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-06-08 08:52:26 -0400

    Add: dynamicArray - submission-1

---
+++ Data Structures & Algorithms/dynamicArray/submission-1.py
@@ -0,0 +1,40 @@
+class DynamicArray:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.size = 0
+        self._array = [None] * self.capacity
+
+    def get(self, i: int) -> int:
+        return self._array[i]
+
+    def set(self, i: int, n: int) -> None:
+        self._array[i] = n
+
+    def pushback(self, n: int) -> None:
+        if self.size == self.capacity:
+            self.resize()
+        self._array[self.size] = n
+        self.size += 1
+
+    def popback(self) -> int:
+        self.size -= 1
+        return self._array[self.size]
+
+    def resize(self) -> None:
+        """
+            a loop-based copy or list comprehension into a new fixed-size 
+            structure is the classic way to teach this concept.
+        """
+        # self._array = self._array + [None] * self.capacity  
+        self.capacity = self.capacity * 2
+        new_array = [None] * self.capacity
+        for i, el in enumerate(self._array):
+            new_array[i] = el
+        self._array = new_array
+        
+
+    def getSize(self) -> int:
+        return self.size
+
+    def getCapacity(self) -> int:
+        return self.capacity
