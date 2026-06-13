5fec6bed33924c4e3cd4ff0c5db7393c0fa6a38a
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-06-13 17:18:52 -0400

    Add: queue - submission-1

---
+++ Data Structures & Algorithms/queue/submission-1.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value, next=None, previous=None):
+        self.value = value
+        self.next = next
+        self.previous = previous
+
+
+class Deque:
+    def __init__(self):
+        self.start = Node(-1)
+        self.end = Node(-1, previous=self.start)
+        self.start.next = self.end
+
+    def isEmpty(self) -> bool:
+        return self.start.next == self.end
+
+    def append(self, value: int) -> None:
+        last_node = self.end.previous
+        new_node = Node(value=value, previous=last_node, next=self.end)
+        self.end.previous = new_node
+        last_node.next = new_node
+
+    def appendleft(self, value: int) -> None:
+        first_node = self.start.next
+        new_node = Node(value, previous=self.start, next=first_node)
+        self.start.next = new_node
+        first_node.previous = new_node
+
+    def pop(self) -> int:
+        if self.isEmpty():
+            return -1
+        last_node = self.end.previous
+        previous_node = last_node.previous
+        previous_node.next = self.end
+        self.end.previous = previous_node
+        return last_node.value
+
+    def popleft(self) -> int:
+        if self.isEmpty():
+            return -1
+        first_node = self.start.next
+        next_node = first_node.next
+        next_node.previous = self.start
+        self.start.next = next_node
+        return first_node.value
