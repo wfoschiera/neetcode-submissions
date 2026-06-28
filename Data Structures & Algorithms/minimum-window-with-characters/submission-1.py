8b0d685300fa6c92c648f183f1afded0f2a95c25
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-06-28 17:58:36 -0400

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,41 @@
+from collections import Counter
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # we iterate over every char in s using the R pointer
+        # keeping a hash map to compare against count_t (the goal string)
+        # if a char is in count_t and with same qty, we add to have += 1
+        # when we get the equality "need == have", we start subtracing the
+        # result string, this time, using the L pointer.
+        # we will subtract until have == need. So, this way, 
+        # we get the shortest string possible
+        if t == "":
+            return ""
+
+        count_t = Counter(t)
+        l = 0
+        window = {}
+
+        have, need = 0, len(count_t)
+        res, res_len = [-1, -1], float("infinity")
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            # if exists and same quantity
+            if c in count_t and window[c] == count_t[c]:
+                have += 1
+
+            while have == need:
+                if (r - l + 1) < res_len:
+                    res = [l, r]
+                    res_len = r - l + 1
+                window[s[l]] -= 1
+
+                if s[l] in count_t and window[s[l]] < count_t[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if res_len != float("infinity") else ""
