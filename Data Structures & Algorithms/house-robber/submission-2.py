184c6e22b54ca06f7f035722957ab11417ee7a91
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-08-31 19:45:52 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # if len(nums) < 1:
+        #     return 0
+        # return max(nums[0] + self.rob(nums[2:]), self.rob(nums[1:]))
+        rob1 = rob2 = 0
+
+        for n in nums:
+            # pick one or another, avoid neighbors
+            temp = max(n+rob1, rob2)
+            # every n position save the previous sum
+            rob1 = rob2
+            rob2 = temp
+        return rob2
