9acd8d22de9fb56394d0c411d7fd71e82f2ea191
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-09-15 07:29:58 -0400

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def helper(open_p: int, close_p: int, cur_comb: str):
+            # if len(cur_comb) == 2 * n
+            # using this condition we make sure that cases like "(("
+            # for n=2 will not be possible
+            if close_p == open_p == n:
+                res.append(cur_comb)
+
+            if close_p > open_p or open_p > n:
+                return
+
+            # optimization:
+            # avoid exploring branches when 
+            # open_p > n or close_p > open_p
+            if open_p < n:
+                helper(open_p+1, close_p, cur_comb+"(")
+            if close_p < open_p:
+                helper(open_p, close_p+1, cur_comb+")")
+
+        helper(0, 0, "")
+
+        return res
