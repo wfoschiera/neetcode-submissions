ff35209bceedc8e9af2c405bb896eaf0115cf14e
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-09-17 08:35:24 -0400

    Add: sql-default-values - submission-0

---
+++ SQL/sql-default-values/submission-0.sql
@@ -0,0 +1,26 @@
+CREATE TABLE videos(
+    id INTEGER,
+    name TEXT default 'Untitled',
+    is_published BOOLEAN default false
+
+);
+
+
+
+
+
+
+
+-- Do not modify below this line --
+INSERT INTO videos (id, name, is_published) 
+VALUES (1, 'My Video', true),
+       (2, 'Another Video', false);
+
+INSERT INTO videos (id)
+VALUES (3),
+       (4);
+
+INSERT INTO videos (name)
+VALUES ('Video with no ID');
+
+SELECT * FROM videos;
